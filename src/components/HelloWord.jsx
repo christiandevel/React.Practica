d19a3e643b46798cbc6c7160471381c6223da4af
ChristianMorenoCd <christianm.code@gmail.com>
2020-08-15 01:24:11 -0500

    ESlint

---
+++ src/components/HelloWord.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import '../assets/styles/App.scss';
+
+const HelloWord = () => <h1>Hola Munnnnnnnnnnnndo</h1>;
+
+export default HelloWord;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import HelloWord from './components/HelloWord';
+
+// Componente, donde Lo voy a Empujar
+ReactDOM.render(<HelloWord/>, document.getElementById('app'))
